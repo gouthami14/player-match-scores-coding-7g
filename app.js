76847e691f19d51438f8f296505aa3f137184334
gouthami14 <92106741+gouthami14@users.noreply.github.com>
2023-05-20 13:49:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 
-const { open } = require("sqlite3");
+const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 
 const app = express();
